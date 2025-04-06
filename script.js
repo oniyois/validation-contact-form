eacb4e795b58fa5e53b7f0eb5171cd63865c1653
oniyois <oniyoislamiyat@gmail.com>
2025-04-06 22:57:08 +0100

    complete error state

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let firstName = document.getElementById("input1");
 
 let lastName = document.getElementById("input2");
 let email = document.getElementById("input3");
+let checkbox = document.getElementById("checkbox");
 
 let submitButton = document.getElementById("submit-btn");
 submitButton.addEventListener("click", (event) => {
@@ -23,6 +24,14 @@ submitButton.addEventListener("click", (event) => {
     errorMessage3.appendChild(errorMessageTextNode3);
     errorMessage3.style.color = "red";
     email.parentNode.appendChild(errorMessage3);
-  } else {
+  } else if(!checkbox.checked){
+    let errorMessage3 = document.createElement("p");
+    let errorMessageTextNode3 = document.createTextNode(
+      "To submit this form, please consent to being contacted"
+    );
+
+
+  }else{
+    
   }
 });
